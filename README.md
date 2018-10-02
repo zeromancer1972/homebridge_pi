442a9d9d6829ff46f32065507e4b54c21f9093d4
Oliver Busse <obusse@googlemail.com>
2018-10-02 19:07:27 +0200

    zero script, readme

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # homebridge_pi
 
-Check file ```homebridge_pi.md``` for details
+This is a how to for the installation of Homebridge on a Raspberry Pi. Homebridge allows it to use various smart home components in Apple Homekit even when they are not officially certified for it. For more informations on Homebridge checkout the repo: [https://github.com/nfarina/homebridge](https://github.com/nfarina/homebridge)
+
+For a manual installation check out the file [homebridge_pi.md](homebridge_pi.md) for details.
+
+For an automated installation you need at least a real root account to execute the install scripts. Follow the manual instructions to setup a root account.
+
+On a regular Raspberry Pi (Model B, 3, +) use the [install_pi.sh](install_pi.sh) script. For a Raspberry Pi Zero W use [install_zero.sh](install_zero.sh)
---
+++ install_zero.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# update system
+echo "******** Updating the system"
+apt-get update && apt-get upgrade -y
+echo "******** Installing the video codec"
+apt install ffmpeg -y
+echo "******** Installing Node.js"
+wget https://nodejs.org/dist/v8.12.0/node-v8.12.0-linux-armv6l.tar.xz
+tar xvf node*.tar
+cp -r node*/* /usr/local
+echo "******** Installing Node.js packages for Homebridge"
+npm install -g --unsafe-perm pm2 homebridge homebridge-config-ui-x homebridge-camera-rpi homebridge-camera-ffmpeg
+echo "******** Setting video user rights"
+usermod -aG video pi
+usermod -aG video root
+echo "******** Adding video module"
+echo bcm2835-v4l2 >> /etc/modules
+echo "******** Creating config.json"
+mkdir /root/.homebridge
+cp config.json /root/.homebridge
+echo "******** Setting up Homebridge as a service with pm2"
+pm2 start "homebridge -I"
+pm2 save
+pm2 startup
+echo "******** Finished. You should reboot now"
